e40f527eb1767404f5d3f6234c5531293728b610
Arthur-Olivier Fortin <aofor@ulaval.ca>
2022-12-06 21:13:39 -0500

    Début de graphique

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from utilitaire import analyser_commande, formater_les_parties
 from quoridor_serveur import formater_jeu, récupérer_le_coup, formater_les_parties
 import quoridor_serveur_vs_bot as Q_SvB
 import time
-
+import quoridorx
 # Mettre ici votre secret récupéré depuis le site de PAX
 secret = "07088f23-df64-4ff3-a352-08cb9a19158d"
 
 if __name__ == "__main__":
     args = analyser_commande()
-    
+        
+
     if args.automatique:
         # Implémenter la boucle pour jouer contre le bot du serveur
         id_partie, état = débuter_partie(args.idul, secret)
@@ -56,7 +57,21 @@ if __name__ == "__main__":
                 joueurs, murs = Quoridor.placer_un_mur(Quoridor, 1, position, type_coup)
             Quoridor.est_terminée(Quoridor)
             #joueurs, murs = Quoridor.jouer_le_coup(Quoridor, 2)
-
+    
+    if args.graphique:
+        joueurs = [{"nom": args.idul, "murs": 10, "pos": [5, 1]}, {"nom": 'automate', "murs": 10, "pos": [5, 9]}]
+        murs = {"horizontaux": [],
+        "verticaux": [],}
+        def jouer(joueurs, murs):
+            état = Quoridor.vérification(Quoridor, joueurs, murs)
+            quoridorx.QuoridorX.graphique(état)
+            type_coup, position = Quoridor.récupérer_le_coup(Quoridor, 1)
+            if type_coup == 'D':
+                Quoridor.déplacer_jeton(Quoridor, 1, position)
+            if type_coup == 'MH' or type_coup == 'MV':
+                joueurs, murs = Quoridor.placer_un_mur(Quoridor, 1, position, type_coup)
+            Quoridor.est_terminée(Quoridor)
+            #joueurs, murs = Quoridor.jouer_le_coup(Quoridor, 2)
 
             jouer(joueurs, murs)
         jouer(joueurs, murs)
