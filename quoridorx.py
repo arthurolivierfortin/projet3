76763dd38ffa89747d07d966544f80c902c31ad4
Arthur-Olivier Fortin <aofor@ulaval.ca>
2022-12-06 21:14:53 -0500

    rien a dire

--- quoridorx.py
+++ quoridorx.py
@@ -4,7 +4,7 @@ import turtle
 class QuoridorX(Quoridor):
     
     def graphique(self):
-        fen = turtle.screen()
+        fen = turtle.Screen()
         fen.title("Quorido phase 3")
         fen.setup(width=800, height=600)
         alex = turtle.Turtle()
