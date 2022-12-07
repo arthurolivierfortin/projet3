1f8c8cc8035936d9917c16efcf8f4a08befee6fb
Arthur-Olivier Fortin <aofor@ulaval.ca>
2022-12-06 21:53:08 -0500

    modification dans turtle

--- quoridorx.py
+++ quoridorx.py
@@ -8,6 +8,8 @@ class QuoridorX(Quoridor):
         fen.title("Quorido phase 3")
         fen.setup(width=800, height=600)
         alex = turtle.Turtle()
+        #création de l'arrière plan noir
+        alex.speed("fast")
         alex.fillcolor("black")
         alex.begin_fill()
         alex.forward(400)
@@ -18,5 +20,37 @@ class QuoridorX(Quoridor):
         alex.left(90)
         alex.forward(600)
         alex.left(90)
-        alex.forward(900)
+        alex.forward(800)
+        alex.left(90)
+        alex.forward(300)
         alex.end_fill()
+
+        #périmètre du jeu
+        clair = turtle.Turtle()
+        clair.shape("dot")
+        clair.pencolor("white")
+        clair.penup()
+        clair.forward(200)
+        clair.left(90)
+        clair.pendown()
+        clair.forward(200)
+        clair.left(90)
+        clair.forward(400)
+        clair.left(90)
+        clair.forward(400)
+        clair.left(90)
+        clair.forward(400)
+        clair.left(90)
+        clair.forward(200)
+        clair.left(90)
+        clair.forward(200)
+        clair.pensize(4)
+
+        #point dans le jeu
+        
+        for i in range(10**2):
+            i += 1
+            clair.forward(40*i)
+            clair.right(90)
+            
+        
